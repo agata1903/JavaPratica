b70eb3199041cc009518f9f0194fbcf8727a809d
Ágata <59833071+agata1903@users.noreply.github.com>
2025-02-19 09:42:46 -0300

    Add files via upload

--- JavaPOO/src/encapsulamento/Controle.java
+++ JavaPOO/src/encapsulamento/Controle.java
@@ -38,51 +38,70 @@ public class Controle implements Controlador{
 
     @Override
     public void ligar() {
-        
+        this.setLigado(true);
     }
 
     @Override
     public void desligar() {
-
+        this.setLigado(false);
     }
 
     @Override
     public void abrirMenu() {
+        System.out.println("Está ligado? " + this.getLigado());
+        System.out.println("Está tocando? " + this.getTocando());
+        System.out.print("Volume: " + this.getVolume() + " ");
+        for (int i = 0; i < getVolume(); i +=10) {
+            System.out.print("|");
+        }
 
     }
 
     @Override
     public void fecharMenu() {
-
+        System.out.println();
+        System.out.println("Fechando Menu...");
     }
 
     @Override
     public void aumentarVolume() {
-
+        if (this.getLigado()) {
+            this.setVolume(this.getVolume() + 5);
+        }
     }
 
     @Override
     public void diminuirVolume() {
-
+        if (this.getLigado()) {
+            this.setVolume(this.getVolume() - 5);
+        }
     }
 
     @Override
     public void ativarMudo() {
-
+        if (this.getLigado() && this.getVolume() > 0) {
+            this.setVolume(0);
+        }
     }
 
     @Override
     public void desativarMudo() {
-
+        if (this.getLigado() && this.getVolume() == 0) {
+            this.setVolume(50);
+        }
     }
 
     @Override
     public void play() {
-
+        if (this.getLigado() && !(this.getTocando())) {
+            this.setTocando(true);
+        }
     }
 
     @Override
     public void pause() {
-
+        if (this.getLigado() && this.getTocando()) {
+            this.setTocando(false);
+        }
     }
 }
--- JavaPOO/src/encapsulamento/ControlePrincipal.java
+++ JavaPOO/src/encapsulamento/ControlePrincipal.java
@@ -4,6 +4,13 @@ public class ControlePrincipal {
 
     public static void main(String[] args) {
 
-
+        Controle c = new Controle();
+        c.ligar();
+        c.play();
+        c.aumentarVolume();
+        c.ativarMudo();
+        c.desativarMudo();
+        c.abrirMenu();
+        c.fecharMenu();
     }
 }
