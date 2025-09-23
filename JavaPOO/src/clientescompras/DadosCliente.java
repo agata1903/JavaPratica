7e101ff1aede3ed5112db336a273884e350c3976
Ágata <59833071+agata1903@users.noreply.github.com>
2025-09-23 10:39:04 -0300

    Add files via upload

---
+++ JavaPOO/src/clientescompras/DadosCliente.java
@@ -0,0 +1,43 @@
+package poo.clientescompras;
+
+import java.text.Normalizer;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DadosCliente {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Cliente c1 = new Cliente("Agata", "agata@gmail.com", new ArrayList<>());
+
+        ArrayList<Cliente> amznCliente = new ArrayList<>();
+
+        c1.adicionarCompra("Blusinha");
+
+        amznCliente.add(c1);
+
+        System.out.println("Qual cliente quer pesquisar? ");
+        String escolha = scanner.next();
+        escolha = escolha.substring(0, 1).toUpperCase() + escolha.substring(1).toLowerCase();
+        escolha = Normalizer.normalize(escolha, Normalizer.Form.NFD).replaceAll("\\p{M}", "");
+
+        if (!escolha.matches("[a-zA-Z ]+")) {
+            System.out.println("Nome inválido! Não é permitido usar números, acentos ou caracteres especiais.");
+        }
+        boolean encontrado = false;
+
+        for (Cliente c : amznCliente) {
+            if (c.nome.equalsIgnoreCase(escolha)) {
+                encontrado = true;
+            }
+            if (encontrado) {
+                c.mostrarCompras();
+            }
+        }
+        if (!encontrado) {
+            System.out.println("Cliente não encontrado!");
+        }
+    }
+}
